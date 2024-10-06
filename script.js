4a4f9ff0ce4521b7f4d726302c7265a36728ab98
Iflaq khurshid <112414810+Iflaqbhat@users.noreply.github.com>
2024-10-06 12:46:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 function shortcut(s1, s2) {
   // your code here
-	if (s1.charAt(0)||s2.charAt(0)=="") {
-		console.log('');
+	if (s1.charAt(0)==""||s2.charAt(0)=="") {
+		return '';
 	}
 	else{
 	let a=s1.chatAt(0)+s2.charAt(0);
-	console.log(a) ;
+	return a;
 	}
 }
 
 // Do not change the code below.
-const s1 = prompt("Enter s1:");
-const s2 = prompt("Enter s2:");
-alert(shortcut(s1, s2));
+// const s1 = prompt("Enter s1:");
+// const s2 = prompt("Enter s2:");
+// alert(shortcut(s1, s2));
